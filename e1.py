a46337d70dcbb72768d836dca2e8163a20171fbf
daniel.arco.7e5 <daniel.arco.7e5@itb.cat>
2023-12-01 13:05:35 +0100

    dsds

--- e1.py
+++ e1.py
@@ -10,8 +10,27 @@ quants són positius, quants negatius i quants zero.
 """
 
 try:
-
+    numero = input().split()
+    longuitud = len(numero)
+    positiu = 0
+    negatiu = 0
+    casSigui0 = 0
+    cont = 0
+    if longuitud == 10:
+        while cont != longuitud:
+            if int(numero[cont]) > 0:
+                positiu += 1
+            elif int(numero[cont]) < 0:
+                negatiu += 1
+            elif int(numero[cont]) == 0:
+                casSigui0 += 1
+            else:
+                print("Dades incorrectes, torna a provar amb un enter")
+            cont += 1
+        print("Números positius: {}\n\nNúmeros negatius: {}\n\nNúmeros igual a 0: {}".format(positiu, negatiu, casSigui0))
+    else:
+        print("La longuitud debe ser igual a 10 y solamente numeros enteros")
 except ValueError:
     print("Introduce bien los datos")
 finally:
-    print("Programa terminado")
+    print("\nPrograma terminado")
